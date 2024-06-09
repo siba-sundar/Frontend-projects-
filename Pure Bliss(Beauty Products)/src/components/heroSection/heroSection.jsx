636a22746323a1f84a1ed7944ef7d5e9f4049f50
Siba Sundar <siba.edu2003@gmail.com>
2024-06-09 13:09:42 +0530

    update

--- Pure Bliss(Beauty Products)/src/components/heroSection/heroSection.jsx
+++ Pure Bliss(Beauty Products)/src/components/heroSection/heroSection.jsx
@@ -3,11 +3,11 @@ import "./heroSection.css"
 const HeroSection = () =>{
     return (
         <>
-        <div id="mainHeroContainer">
-            <p className="">Original Sunglasses</p>
-            <h1>Original Sunglasses</h1>
+        <div className="mainHeroContainer">
+            <p className="p1">Original Beauty</p>
+            <h1>Beauty Collection</h1>
 
-            <p>Fusce scelerisque tellus eget nisl vulputate, et eleifend sapien tempor nulla tristique.</p>
+            <p className="p2">Fusce scelerisque tellus eget nisl vulputate, et eleifend sapien tempor nulla tristique.</p>
 
             <button>Shop Now </button>
         </div>
